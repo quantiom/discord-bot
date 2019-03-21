70672b51bd001f0aea98d426fb57315091390e05
quantiom <35455461+quantiom@users.noreply.github.com>
2019-03-20 21:47:28 -0400

    updated help command
    updated help command

--- commands/info/help.js
+++ commands/info/help.js
@@ -18,16 +18,11 @@ module.exports = class extends Command
     {
         let helpEmbed = new RichEmbed()
             .setAuthor(client.user.username, client.user.displayAvatarURL)
-            .setDescription(`Use \`${config.prefix}help <command name>\` to get information on a certain command.\n\`<param>\` = Required\n\`|param|\` = Optional`)
-            .setColor(0xd83232)
-            .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL);
+            .setDescription(`Prefix: \`${config.prefix}\`\nUse \`${config.prefix}help <command name>\` to get information on a certain command.`)
+            .setColor(0xd83232);
 
-        Handler.categories.keyArray().forEach(cat => {
-            let toAdd = '';
-            Handler.categories.get(cat).forEach(cmd => {
-                toAdd += cmd.name;
-            })
-            helpEmbed.addField(cat, toAdd);
+        Handler.categories.forEach((commands, category) => {
+            helpEmbed.addField(`${category} (${commands.length})`, commands.map(cmd => `\`${cmd.name}\``).join('\n'));
         });
 
         message.channel.send(helpEmbed);
