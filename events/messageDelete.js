b7680dc8621de35fc45fa7801fabf7fc5dd25579
quantiom <35455461+quantiom@users.noreply.github.com>
2019-03-22 20:55:41 -0400

    added picture logging to deleted messages
    picture logging to deleted messages

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -6,6 +6,9 @@ module.exports = async (client, message) => {
     if (message.channel.type == 'text')
     Utils.logCheck(message.guild, 'messageDeletes').then(logChannel => {
         if (!logChannel) return;  
-        logChannel.send(Utils.embed("", message.content).setAuthor("Message Deleted", message.author.displayAvatarURL).setTimestamp().setFooter(`Sent by ${message.author.tag} (${message.author.id}) in #${message.channel.name}`));
+        let logEmbed = Utils.embed("", message.content).setAuthor("Message Deleted", message.author.displayAvatarURL).setTimestamp().setFooter(`Sent by ${message.author.tag} (${message.author.id}) in #${message.channel.name}`);
+        if (message.attachments.first() && message.attachments.first().proxyURL)
+        logEmbed.setImage(message.attachments.first().proxyURL);
+        logChannel.send(logEmbed);
     });
 }
